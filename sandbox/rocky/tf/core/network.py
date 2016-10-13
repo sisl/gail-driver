9d172dead42c6d6c7ddac250b8dde3ed670e21ad
Alex Kuefler <kuefleralex@gmail.com>
2016-10-13 10:47:15 -0700

    Added wrappers for baseline and reward networks.

--- sandbox/rocky/tf/core/network.py
+++ sandbox/rocky/tf/core/network.py
@@ -6,57 +6,69 @@ from rllab.core.serializable import Serializable
 from sandbox.rocky.tf.core.parameterized import Parameterized
 from sandbox.rocky.tf.core.layers_powered import LayersPowered
 
+from rllab.baselines.base import Baseline
+from rllab.misc.overrides import overrides
+
+from sandbox.rocky.tf.optimizers.first_order_optimizer import Solver
+from sandbox.rocky.tf.optimizers.lbfgs_optimizer import LbfgsOptimizer
+
 class NeuralNetwork(object):
-    
+
     def _predict(self, t, X):
         sess = tf.get_default_session()
-        
+
         N, _ = X.shape
         B = self.input_var.get_shape()[0].value
-        
+
         if B is None or B == N:
             pred = sess.run(t, {self.input_var: X})
         else:
             pred = [sess.run(t, {self.input_var: X[i:i+B]}) for i in range(0,N,B)]
             pred = np.row_stack(pred)
-            
+
         return pred
-    
+
     def likelihood_loss(self):
         if self.output_layer.nonlinearity == tf.nn.softmax:
             logits = self.output_layer.get_logits_for(L.get_output(self.layers[-2]))
             loss = tf.reduce_mean(
                 tf.nn.softmax_cross_entropy_with_logits(logits, tf.squeeze(self.target_var))
                 )
-            
+
         elif self.output_layer.nonlinearity == tf.identity:
             outputs = self.output_layer.get_output_for(L.get_output(self._layers[-2]))
             loss = tf.reduce_mean(
                 0.5 * tf.square(outputs - self.target_var), name='like_loss'
             )
-        
+
+        elif self.output_layer.nonlinearity == tf.sigmoid:
+            logits = self.output_layer.get_logits_for(L.get_output(self.layers[-2]))
+            loss = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(logits, tf.squeeze(self.target_var))
+                )
+
         return loss
-    
+
     def complexity_loss(self, reg, cmx):
         """
         Compute penalties for model complexity (e.g., l2 regularization, or kl penalties for vae and bnn).
         """
         # loss coming from weight regularization
         loss = reg * tf.reduce_sum(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
-        
+
         # loss coming from data-dependent regularization
         for layer in self.layers:
             if layer.penalize_complexity:
                 z_mu, z_sig = layer.get_dparams_for(L.get_output(layer.input_layer))
                 d_loss = layer.bayesreg.activ_kl(z_mu,z_sig)
-                
+
                 loss += cmx * d_loss
-            
+
         return reg * loss
-    
+
     def loss(self, reg= 0.0, cmx= 1.0):
         return tf.add(self.likelihood_loss(), self.complexity_loss(reg, cmx),name= 'loss')
-    
+
     @property
     def input_layer(self):
         return self._l_in
@@ -68,10 +80,10 @@ class NeuralNetwork(object):
     @property
     def input_var(self):
         return self._l_in.input_var
-    
+
     @property
     def target_var(self):
-        return self._l_tar.input_var       
+        return self._l_tar.input_var
 
     @property
     def layers(self):
@@ -80,29 +92,29 @@ class NeuralNetwork(object):
     @property
     def output(self):
         return self._output
-    
+
     @property
     def n_params(self):
         return sum([np.prod(param.get_shape()).value for param in self.get_params()])
-    
-    
+
+
 class DeterministicNetwork(NeuralNetwork):
-    
+
     def predict(self, X):
-        
+
         if self.output_layer.nonlinearity == tf.nn.softmax:
             y_p = tf.argmax(self._output,1)
         else:
-            y_p = self._output      
-        
+            y_p = self._output
+
         Y_p = self._predict(y_p, X)
         return Y_p
-    
+
 class StochasticNetwork(NeuralNetwork):
-    
+
     def predict(self, X, k= 1):
         sess = tf.get_default_session()
-        
+
         o_p= []
         for _ in range(k):
 
@@ -110,14 +122,14 @@ class StochasticNetwork(NeuralNetwork):
             o_p = np.concatenate([o[None,...] for o in o_p], axis= 0)
             mu_p = np.mean(o_p,axis= 0)
             std_p = np.std(o_p,axis= 0)
-        
+
         if self.output_layer.nonlinearity == tf.nn.softmax:
             Y_p = np.argmax(mu_p,1)
         elif self.output_layer.nonlinearity == tf.identity:
             Y_p = mu_p
-        
-        return Y_p 
-    
+
+        return Y_p
+
 
 class MLP(LayersPowered, Serializable, DeterministicNetwork):
     def __init__(self, name, output_dim, hidden_sizes, hidden_nonlinearity,
@@ -166,13 +178,13 @@ class MLP(LayersPowered, Serializable, DeterministicNetwork):
             self._l_in = l_in
             self._l_out = l_out
             self._l_tar = L.InputLayer(shape=(batch_size,) + (output_dim,), input_var=input_var, name="target")
-            
+
             # self._input_var = l_in.input_var
             self._output = L.get_output(l_out)
 
             LayersPowered.__init__(self, l_out)
-    
-    
+
+
 class BayesMLP(LayersPowered, Serializable, StochasticNetwork):
     def __init__(self, name, output_dim, hidden_sizes, hidden_nonlinearity,
                  output_nonlinearity, hidden_W_init=L.XavierUniformInitializer(), hidden_b_init=tf.zeros_initializer,
@@ -187,7 +199,7 @@ class BayesMLP(LayersPowered, Serializable, StochasticNetwork):
         with tf.variable_scope(name):
             if input_layer is None:
                 l_in = L.InputLayer(shape=(batch_size,) + input_shape, input_var=input_var, name="input")
-                
+
             else:
                 l_in = input_layer
             self._layers = [l_in]
@@ -224,13 +236,13 @@ class BayesMLP(LayersPowered, Serializable, StochasticNetwork):
             self._l_in = l_in
             self._l_out = l_out
             self._l_tar = L.InputLayer(shape=(batch_size,) + (output_dim,), input_var=input_var, name="target")
-            
+
             # self._input_var = l_in.input_var
             self._output = L.get_output(l_out)
 
             LayersPowered.__init__(self, l_out)
-            
-            
+
+
 class LatentMLP(LayersPowered, Serializable, StochasticNetwork):
     def __init__(self, name, output_dim, hidden_spec, hidden_nonlinearity,
                  output_nonlinearity, hidden_W_init=L.XavierUniformInitializer(), hidden_b_init=tf.zeros_initializer,
@@ -262,7 +274,7 @@ class LatentMLP(LayersPowered, Serializable, StochasticNetwork):
                         b=hidden_b_init,
                         weight_normalization=weight_normalization,
                         reg_params= reg_params
-                    )                    
+                    )
 
                 elif hidden_type == 'latent':
                     l_hid = L.LatentLayer(
@@ -273,7 +285,7 @@ class LatentMLP(LayersPowered, Serializable, StochasticNetwork):
                         b=hidden_b_init,
                         weight_normalization=weight_normalization,
                         reg_params= reg_params
-                    )                    
+                    )
 
                 else:
                     raise NotImplementedError
@@ -297,19 +309,82 @@ class LatentMLP(LayersPowered, Serializable, StochasticNetwork):
             self._l_in = l_in
             self._l_out = l_out
             self._l_tar = L.InputLayer(shape=(batch_size,) + (output_dim,), input_var=input_var, name="target")
-            
+
             # self._input_var = l_in.input_var
             self._output = L.get_output(l_out)
 
             LayersPowered.__init__(self, l_out)
-            
+
     def sample(self):
         sess = tf.get_default_session()
         l = sess.run(
             L.get_output([layer for layer in self.layers], inputs= tf.random_uniform(self._l_in.input_shape))
         )
-        
+
         return l[-1]
+
+class RewardMLP(MLP):
+    """
+    overrides MLP with methods / properties used in generative adversarial learning.
+    """
+
+    def compute_reward(self, X):
+        predits = -tf.log(1.0 - self.output)
+        Y_p = self._predict(predits, X)
+        return Y_p
+
+    def compute_score(self, X):
+        """
+        predict logits ...
+        """
+        logits = self.output_layer.get_logits_for(L.get_output(self.layers[-2]))
+        Y_p = self._predict(logits, X)
+        return Y_p
+
+    def likelihood_loss(self):
+        logits = self.output_layer.get_logits_for(L.get_output(self.layers[-2]))
+        loss = tf.nn.sigmoid_cross_entropy_with_logits(logits, self.target_var)
+        #ent_B = tfutil.logit_bernoulli_entropy(logits)
+        #self.obj = tf.reduce_sum(loss_B - self.ent_reg_weight * ent_B)
+        return tf.reduce_sum(loss)
+
+    def complexity_loss(self, reg, cmx):
+        return tf.constant(0.0)
+
+    def loss(self, reg= 0.0, cmx= 0.0):
+        #logits = self.output_layer.get_logits_for(L.get_output(self.layers[-2]))
+        #loss = tf.nn.sigmoid_cross_entropy_with_logits(logits, self.target_var)
+        #ent_B = tfutil.logit_bernoulli_entropy(logits)
+        #self.obj = tf.reduce_sum(loss_B - self.ent_reg_weight * ent_B)
+        #return tf.reduce_sum(loss)
+        loss = self.likelihood_loss()
+        return loss
+
+class BaselineMLP(MLP, Baseline):
+    def initialize_optimizer(self):
+        self._optimizer = LbfgsOptimizer('optim')
+
+        optimizer_args = dict(
+            loss=self.loss(),
+            target=self,
+            inputs = [self.input_var, self.target_var],
+            network_outputs=[self.output]
+        )
+
+        self._optimizer.update_opt(**optimizer_args)
+
+    @overrides
+    def predict(self, path):
+        # X = np.column_stack((path['observations'], path['actions']))
+        X = path['observations']
+        return super(BaselineMLP, self).predict(X)
+
+    @overrides
+    def fit(self, paths):
+        observations = np.concatenate([p["observations"] for p in paths])
+        returns = np.concatenate([p["returns"] for p in paths])
+        #self._regressor.fit(observations, returns.reshape((-1, 1)))
+        self._optimizer.optimize([observations, returns[...,None]])
 
 
 class ConvNetwork(LayersPowered, Serializable):
