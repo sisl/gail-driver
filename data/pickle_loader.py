e38d5a0f73edb225707b77a9ca821470772a6f11
Alex Kuefler <kuefleralex@gmail.com>
2016-11-21 09:13:17 -0800

    blah

--- data/pickle_loader.py
+++ data/pickle_loader.py
@@ -3,12 +3,15 @@ import rltools.util
 import joblib
 from rltools.envs.julia_sim import JuliaEnvWrapper
 import tensorflow as tf
+import numpy as np
 
 import h5py
 
+import sandbox.rocky.tf.core.layers as L
+
 from rllab.sampler.utils import rollout
 
-env_dict = {'trajdata_indeces': [3],
+env_dict = {'trajdata_indeces': [1],
         'use_playback_reactive': True,
         'extract_core':True,
         'extract_temporal':False,
@@ -30,23 +33,32 @@ gym.envs.register(
 
 
 with tf.Session() as sess:
-	x = joblib.load('16-11-04/radar_gru-0/itr_450.pkl')
+	x = joblib.load('16-11-04/radar_gru-3/itr_417.pkl') # 417 looked good
 	pi = x['policy']
-	halt= True
-	key = 'iter{:05}/'.format(0)
-	with h5py.File('pleasework.h5', 'a') as hf:
-		if key in hf:
-			dset = hf[key]
-		else:
-			dset = hf.create_group(key)
-		vs = pi.get_params()
-		vals = sess.run(vs)
+	#halt= True
+	#key = 'iter{:05}/'.format(0)
+	#with h5py.File('goodpolicy.h5', 'a') as hf:
+		#if key in hf:
+			#dset = hf[key]
+		#else:
+			#dset = hf.create_group(key)
+		#vs = pi.get_params()
+		#vals = sess.run(vs)
 
-		for v, val in zip(vs, vals):
-			dset[v.name] = val
+		#for v, val in zip(vs, vals):
+			#dset[v.name] = val
+	#halt= True
+	#activations = []
+	#z = np.zeros((1,51))
+	#with h5py.File('activations.h5', 'a') as hf:
+	#	for layer in pi.feature_network.layers[1:]:
+	#		a = sess.run(L.get_output(layer),
+	#		             {pi.feature_network.input_var: z})
+	#		hf.create_dataset(layer.name, data=a)
 
-	#while True:
-		#rollout(x['env'],x['policy'],max_path_length=100,animated=True)
+
+	while True:
+		rollout(x['env'],x['policy'],max_path_length=100,animated=True)
 
 #policy = x['policy']
 #policy.set_
